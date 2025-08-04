5f01eecf29c696f110831fb15798badd46541ae3
Jonnatan Arias Garcia <137342778+JntnAG@users.noreply.github.com>
2025-08-04 16:54:44 -0500

    logo nebulityka

--- cafe_cata_proyecto/settings.py
+++ cafe_cata_proyecto/settings.py
@@ -116,10 +116,11 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
-
 STATIC_URL = "static/"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "app_cata" / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
